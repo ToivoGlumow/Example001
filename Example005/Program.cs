f071259f6ede2eb8ef2021cd0c19825b283d3d62
Toivo <xan.asur@mail.ru>
2023-07-04 06:42:10 +0300

    Example005-Приветствие

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это Маша!");
+}
+else
+{
+    Console.Write($"Привет, {username} !");
+}
