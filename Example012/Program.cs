09109e5d5db6e1d6c2a06c02998047291ba497de
Toivo <xan.asur@mail.ru>
2023-07-11 04:50:56 +0300

    Example012 Sort

---
+++ Example012/Program.cs
@@ -0,0 +1,34 @@
+﻿
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+int[] arr = { 9, 26, 3, 4, 5, 6, 7, 1, 3 };
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
